80070e0b546f126db7ac20d2a30b4aab31b80180
Thige254 <thigetj@gmail.com>
2023-08-06 06:07:21 -0700

    shell one1

--- interpreter.c
+++ interpreter.c
@@ -7,25 +7,25 @@
  */
 char *read_command(void)
 {
-    char *line = NULL;
-    size_t bufsize = 0;
+	char *line = NULL;
+	size_t bufsize = 0;
 
-    if (getline(&line, &bufsize, stdin) == -1)
-    {
-        if (feof(stdin))
-        {
-            write(STDOUT_FILENO, "\n", 1);
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            perror("readline");
-            exit(EXIT_FAILURE);
-        }
-    }
-    /* Replace newline with null character */
-    line[strlen(line) - 1] = '\0';
-    return (line);
+	if (getline(&line, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("readline");
+			exit(EXIT_FAILURE);
+		}
+	}
+	/* Replace newline with null character */
+	line[strlen(line) - 1] = '\0';
+	return (line);
 }
 
 /**
@@ -37,29 +37,29 @@ char *read_command(void)
  */
 char *get_cmd_path(char *cmd)
 {
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *token = strtok(path_copy, ":");
-    char *cmd_path = malloc(strlen(cmd) + strlen(path) + 2);
-    struct stat buf;
+	char *path = getenv("PATH");
+	char *path_copy = strdup(path);
+	char *token = strtok(path_copy, ":");
+	char *cmd_path = malloc(strlen(cmd) + strlen(path) + 2);
+	struct stat buf;
 
-    while (token != NULL)
-    {
-        strcpy(cmd_path, token);
-        strcat(cmd_path, "/");
-        strcat(cmd_path, cmd);
+	while (token != NULL)
+	{
+		strcpy(cmd_path, token);
+		strcat(cmd_path, "/");
+		strcat(cmd_path, cmd);
 
-        if (stat(cmd_path, &buf) == 0)
-        {
-            free(path_copy);
-            return (cmd_path);
-        }
-        token = strtok(NULL, ":");
-    }
+		if (stat(cmd_path, &buf) == 0)
+		{
+			free(path_copy);
+			return (cmd_path);
+		}
+		token = strtok(NULL, ":");
+	}
 
-    free(cmd_path);
-    free(path_copy);
-    return (NULL);
+	free(cmd_path);
+	free(path_copy);
+	return (NULL);
 }
 
 /**
@@ -68,53 +68,52 @@ char *get_cmd_path(char *cmd)
  */
 void shell_loop(void)
 {
-    char *line;
-    char *command;
-    pid_t child_pid;
-    int status;
+	char *line;
+	char *command;
+	pid_t child_pid;
+	int status;
 
-    while (1)
-    {
-        printf("$ ");
-        line = read_command();
-        command = get_cmd_path(line);
+	while (1)
+	{
+		printf("$ ");
+		line = read_command();
+		command = get_cmd_path(line);
 
-        if (command == NULL)
-        {
-            fprintf(stderr, "%s: command not found\n", line);
-            free(line);
-            continue;
-        }
+		if (command == NULL)
+		{
+			fprintf(stderr, "%s: command not found\n", line);
+			free(line);
+			continue;
+		}
 
-        child_pid = fork();
-        if (child_pid == -1)
-        {
-            perror("Error:");
-            free(line);
-            free(command);
-            continue;
-        }
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			free(line);
+			free(command);
+			continue;
+		}
 
-        if (child_pid == 0)
-        {
-            /* Child process */
-            char *argv[2];
+		if (child_pid == 0)
+		{
+			/* Child process */
+			char *argv[2];
 			argv[0] = command;
 			argv[1] = NULL;
+			if (execve(command, argv, NULL) == -1)
+			{
+				perror("Error:");
+			}
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			/* Parent process */
+			wait(&status);
+		}
 
-            if (execve(command, argv, NULL) == -1)
-            {
-                perror("Error:");
-            }
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            /* Parent process */
-            wait(&status);
-        }
-
-        free(line);
-        free(command);
-    }
+		free(line);
+		free(command);
+	}
 }
