1d4d9a7bb9b67a2a77512964a60d02c5c364713b
Thige254 <thigetj@gmail.com>
2023-11-09 22:42:22 +0300

    exit exit

--- exit.c
+++ exit.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,6 +10,7 @@
  * Description:
  * This function checks if the command is "exit" and, if so, exits the shell.
  * It frees the memory allocated for tokens before exiting.
+ * If "exit" is part of another command, it prints an error message.
  */
 void handle_exit(char **tokens)
 {
@@ -17,5 +19,13 @@ void handle_exit(char **tokens)
 	{
 		free(tokens);
 		exit(0);
+	}
+	else
+	{
+		/* Print an error message for other cases */
+		fprintf(stderr, "Shell: command not found: %s\n", tokens[0]);
+		/* Free the memory allocated for tokens */
+		free(tokens);
+		/* I will add more error handling / execute the command here */
 	}
 }
